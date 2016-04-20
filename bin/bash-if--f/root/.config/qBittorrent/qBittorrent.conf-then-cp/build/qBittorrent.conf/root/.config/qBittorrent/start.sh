f6635299e37812a61b389c71f291e73270379efd
adamrbell <adamrbell@gmail.com>
2016-04-20 18:19:41 +0100

    Delete start.sh

--- bin/bash-if--f/root/.config/qBittorrent/qBittorrent.conf-then-cp/build/qBittorrent.conf/root/.config/qBittorrent/start.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-
-if [ ! -f /root/.config/qBittorrent/qBittorrent.conf ] ; then
-	cp /build/qBittorrent.conf /root/.config/qBittorrent/qBittorrent.conf
-fi
-
-echo "Starting qbittorrent..."
-exec qbittorrent-nox
