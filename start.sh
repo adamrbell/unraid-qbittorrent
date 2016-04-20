873c3214ad2e49dea74604ec892c0bd33498874a
adamrbell <adamrbell@gmail.com>
2016-04-20 18:19:56 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+
+if [ ! -f /root/.config/qBittorrent/qBittorrent.conf ] ; then
+	cp /build/qBittorrent.conf /root/.config/qBittorrent/qBittorrent.conf
+fi
+
+echo "Starting qbittorrent..."
+exec qbittorrent-nox
